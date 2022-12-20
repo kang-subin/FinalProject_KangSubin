0aead6c49c841e754466a3a0675e9633ccdb20af
강수빈 <tnbin98@gmail.com>
2022-12-20 11:20:49 +0900

    hello controller 구현

---
+++ src/main/java/com/example/personalproject/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.personalproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        String message = "hello";
+        return message;
+    }
+
+}
