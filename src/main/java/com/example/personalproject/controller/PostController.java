7dd5d69876a2a03080444f4ed0231c266d96036d
강수빈 <tnbin98@gmail.com>
2022-12-23 00:41:18 +0900

    PostController 수정

--- src/main/java/com/example/personalproject/controller/PostController.java
+++ src/main/java/com/example/personalproject/controller/PostController.java
@@ -6,7 +6,6 @@ import com.example.personalproject.domain.request.UserPostRequest;
 import com.example.personalproject.domain.response.UserPostResponse;
 import com.example.personalproject.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,10 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
-    @PostMapping
-    public Response<UserPostResponse> write (@RequestBody UserPostRequest userPostRequest, Authentication authentication){
-        String name = authentication.getName();
-        PostDto postDto = postService.write(userPostRequest,name);
+    @PostMapping("")
+    public Response<UserPostResponse> write (@RequestBody UserPostRequest userPostRequest){
+        PostDto postDto = postService.write(userPostRequest);
         return Response.success(new UserPostResponse("포스트 등록 완료", postDto.getId()));
     }
 
--- src/main/java/com/example/personalproject/service/PostService.java
+++ src/main/java/com/example/personalproject/service/PostService.java
@@ -1,25 +1,18 @@
 package com.example.personalproject.service;
 
-import com.example.personalproject.JwtTokenUtil.JwtTokenUtil;
 import com.example.personalproject.domain.dto.PostDto;
 import com.example.personalproject.domain.entity.Post;
 import com.example.personalproject.domain.request.UserPostRequest;
-import com.example.personalproject.exception.ErrorCode;
-import com.example.personalproject.exception.UserException;
 import com.example.personalproject.repository.PostRepository;
-import com.example.personalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
 
-    public PostDto write(UserPostRequest userPostRequest, String name) {
-
-        userRepository.findByUserName(name).orElseThrow(()-> new UserException(ErrorCode.INVALID_TOKEN,"잘못된 token 입니다."));
+    public PostDto write(UserPostRequest userPostRequest) {
 
         Post post = Post.builder()
                 .title(userPostRequest.getTitle())
